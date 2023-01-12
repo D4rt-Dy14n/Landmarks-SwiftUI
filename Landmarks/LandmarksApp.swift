581ea31fdb0a0f510736807242a3772efc412035
yr.fedorov <yr.fedorov@andersenlab.com>
2023-01-12 17:34:18 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yuri Fedorov on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
